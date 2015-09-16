0777b65c832f47fe4717ab0974b7896cdc8991d8
Jean Hany <jean_hany@hotmail.com>
2015-09-16 18:57:44 +0200

    add opencv image processing

--- app/src/main/java/palet/jean/distance_palet/ImageProcessing.java
+++ app/src/main/java/palet/jean/distance_palet/ImageProcessing.java
@@ -10,14 +10,43 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt4;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.opencv.core.Core.circle;
+import static org.opencv.core.Core.rectangle;
+import static org.opencv.imgproc.Imgproc.approxPolyDP;
+import static org.opencv.imgproc.Imgproc.blur;
+import static org.opencv.imgproc.Imgproc.boundingRect;
+import static org.opencv.imgproc.Imgproc.cvtColor;
+import static org.opencv.imgproc.Imgproc.drawContours;
+import static org.opencv.imgproc.Imgproc.findContours;
+import static org.opencv.imgproc.Imgproc.minEnclosingCircle;
+import static org.opencv.imgproc.Imgproc.threshold;
 
 /**
  * Created by Jean_jean on 9/15/2015.
  */
 public class ImageProcessing extends Activity implements CameraBridgeViewBase.CvCameraViewListener {
 
+
     private String TAG = "Image processing";
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private int thresh = 100;
+
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
@@ -35,7 +64,7 @@ public class ImageProcessing extends Activity implements CameraBridgeViewBase.Cv
         }
     };
 
-    private CameraBridgeViewBase mOpenCvCameraView;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,6 +75,7 @@ public class ImageProcessing extends Activity implements CameraBridgeViewBase.Cv
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloOpenCvView);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
+
     }
 
     @Override
@@ -81,6 +111,63 @@ public class ImageProcessing extends Activity implements CameraBridgeViewBase.Cv
 
     @Override
     public Mat onCameraFrame(Mat inputFrame) {
-        return null;
+        Mat src_gray = new Mat();
+        Mat threshold_output = new Mat();
+        ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+        MatOfInt4 hierarchy = new MatOfInt4();
+        Log.i(TAG, inputFrame.toString());
+        cvtColor(inputFrame, src_gray, Imgproc.COLOR_BGR2GRAY);
+        Size size = new Size(3, 3);
+        blur( src_gray, src_gray, size );
+        threshold( src_gray, threshold_output, thresh, 255, Imgproc.THRESH_BINARY );
+        /// Find contours
+        findContours( threshold_output, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0, 0) );
+        /// Approximate contours to polygons + get bounding rects and circles
+        ArrayList<MatOfPoint2f> contours_poly = new ArrayList<MatOfPoint2f>();
+        MatOfRect boundRect = new MatOfRect();
+        MatOfPoint2f center = new MatOfPoint2f();
+        MatOfFloat radius = new MatOfFloat( contours.size() );
+
+
+
+        Log.i(TAG, "Size contour :" + Integer.toString(contours.size()));
+        List<MatOfPoint2f> newContours = new ArrayList<MatOfPoint2f>();
+        for(MatOfPoint point : contours) {
+            MatOfPoint2f newPoint = new MatOfPoint2f(point.toArray());
+            newContours.add(newPoint);
+            contours_poly.add(newPoint);
+        }
+
+        List<MatOfPoint> newContours_poly = new ArrayList<MatOfPoint>();
+        for(MatOfPoint2f point : contours_poly) {
+            MatOfPoint newPoint = new MatOfPoint(point.toArray());
+            newContours_poly.add(newPoint);
+        }
+
+        Log.i(TAG, "Size contour :" + Integer.toString(newContours.size())+ " "+ Integer.toString(contours_poly.size()));
+        boundRect.alloc(contours.size());
+        center.alloc(contours.size());
+        radius.alloc(contours.size());
+        Rect[] rect_array = boundRect.toArray();
+
+        for( int i = 0; i < contours.size(); i++ )
+        {
+            approxPolyDP( newContours.get(i), contours_poly.get(i), 3, true );
+            rect_array[i] = boundingRect(newContours_poly.get(i));
+            minEnclosingCircle( contours_poly.get(i), center.toArray()[i], radius.toArray() );
+        }
+
+        boundRect.fromArray(rect_array);
+        /// Draw polygonal contour + bonding rects + circles
+        Mat drawing = new Mat( threshold_output.size(), CvType.CV_8UC3 );
+        for( int i = 0; i< contours.size(); i++ )
+        {
+            Scalar color = new Scalar( 180, 50, 60 );
+            drawContours( drawing, newContours_poly, i, color, 1, 8, new MatOfInt4(), 0, new Point() );
+            rectangle( drawing, boundRect.toArray()[i].tl(), boundRect.toArray()[i].br(), color, 2, 8, 0 );
+            circle( drawing, center.toArray()[i], (int)radius.toArray()[i], color, 2, 8, 0 );
+        }
+
+        return drawing;
     }
 }
